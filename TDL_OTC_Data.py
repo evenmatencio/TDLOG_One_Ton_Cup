ea42a034f92ee6be35ff71945b038ec0b743f740
Prunelle Vogler <prunelle-vogler@orange.fr>
2021-11-22 16:19:32 +0100

    Définition des classes de boatgps à pression atmo

--- TDL_OTC_Data.py
+++ TDL_OTC_Data.py
@@ -15,26 +15,118 @@ class OneTonCupData:
 
 
 class BoatGPSCoordinates(OneTonCupData):
-    def __init__(self):
+    def __init__(self, data_import):
         super().__init__()
-        self.data_import = None
+        self.data_import = data_import
 
-    def load_data(self):
-        self.data_import = pandas.read_csv(os.getcwd() + "/Random_data.csv", delimiter=',', header=0,
-                                           usecols=['Latitude', 'Longitude'])
-    def print_data(self):
-        print(self.data_import)
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import=dataframe["Latitude", "Longitude"]
+        return BoatGPSCoordinates(data_import)
 
 class Depth(OneTonCupData):
-    def __init__(self):
+    def __init__(self, data_import):
         super().__init__()
-        self.data_import = None
+        self.data_import = data_import
 
-    def load_data(self):
-        self.data_import = pandas.read_csv(os.getcwd() + "/Random_data.csv", delimiter=',', header=0,
-                                           usecols=['Profondeur'])
-    def print_data(self):
-        print(self.data_import)
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Profondeur"]
+        return BoatGPSCoordinates(data_import)
+
+
+class Cap(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Cap"]
+        return BoatGPSCoordinates(data_import)
+
+
+class RoadDeviation(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Ecart a la route"]
+        return BoatGPSCoordinates(data_import)
+
+
+class BoatAngles(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Angle de gite","Angle de tangage"]
+        return BoatGPSCoordinates(data_import)
+
+
+class Speeds(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Vitesse bateau", "VMG"]
+        return BoatGPSCoordinates(data_import)
+
+class FlightHeight(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Hauteur de vol"]
+        return BoatGPSCoordinates(data_import)
+
+class WingAngles(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Vrillage angle 1","Vrillage angle 2","Cambrure aile"]
+        return BoatGPSCoordinates(data_import)
+
+class WindAngles(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Angle au vent reel","Angle au vent apparent"]
+        return BoatGPSCoordinates(data_import)
+
+class WindSpeed(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Vitesse du vent","Beaufort"]
+        return BoatGPSCoordinates(data_import)
+
+class Pressure(OneTonCupData):
+    def __init__(self, data_import):
+        super().__init__()
+        self.data_import = data_import
+
+    @staticmethod
+    def from_pandas(dataframe):
+        data_import = dataframe["Pression atmospherique"]
+        return BoatGPSCoordinates(data_import)
 
 coord_test = BoatGPSCoordinates()
 coord_test.load_data()
