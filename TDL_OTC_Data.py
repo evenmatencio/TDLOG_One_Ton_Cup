c4a603bcb3f6d4bc0f7883436ccbf1fc3144978b
Even <matencio.even@gmail.com>
2021-11-22 14:21:45 +0100

    Version fonctionnelle de l'affichage simple des coordonnees GPS du bateau avec un simple print.

---
+++ TDL_OTC_Data.py
@@ -0,0 +1,30 @@
+import os
+import pandas
+from queue import Queue
+
+""" This module contains the parent Data class for the OTC software project
+ and the subclasses related to the different data used in the software """
+
+
+class OneTonCupData:
+    def __init__(self):
+        self.data_table = Queue(maxsize=0)
+
+    def load_data(self):
+        return self.data_table.full
+
+
+class BoatGPSCoordinates(OneTonCupData):
+    def __init__(self):
+        super().__init__()
+        self.data_import = None
+
+    def load_data(self):
+        self.data_import = pandas.read_csv(os.getcwd() + "/Random_data.csv", delimiter=',', header=0,
+                                           usecols=['Latitude', 'Longitude'])
+
+
+coord_test = BoatGPSCoordinates()
+coord_test.load_data()
+
+print(coord_test.data_import)
