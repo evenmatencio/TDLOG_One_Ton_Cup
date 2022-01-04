86e296ba14c4a9d519f04fa54599ce772e96b0b3
Inès Larroche <ines.larroche@gmail.com>
2022-01-04 10:49:58 +0100

    Main du widget AnalogGaugeWidget.py

--- Affichage données Inès/Vitesse vent/speedometer_py2.py
+++ Affichage données Inès/Vitesse vent/speedometer_py2.py
@@ -16,18 +16,21 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(" Apparent Wind Speed")
         
-        ##DEFINING THE WIDGET###
+        ##DEFINING THE W###
         MainWindow.resize(800, 700)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         
         ##ANALOG GAUGE APPARENT WIND SPEED##
         self.centralwidget.setObjectName("centralwidget")
+        
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(120, 120, 541, 491))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        
         self.widget = AnalogGaugeWidget(self.horizontalLayoutWidget)
         self.widget.setObjectName("widget")
         self.horizontalLayout.addWidget(self.widget)
