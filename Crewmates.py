7697a79d3aef1cd456b7155a24d3a91b21c825fc
Even <matencio.even@gmail.com>
2022-01-17 18:36:22 +0100

    Dossiers Helmsman et widgets

--- Crewmates.py
+++ Crewmates.py
@@ -1,4 +1,6 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 import TDL_OTC_Data as Data
+import premiere_interface_menu_inter as HelmsmanWindow
 
 """This module contains the classes for the different members of the sailing team"""
 
@@ -16,10 +18,12 @@ import TDL_OTC_Data as Data
 
 
 class CrewMate:
-    def __init__(self, name, boat_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+
+    def __init__(self, name, temperature, boat_coordinates, depth, buoy_coordinates, stream_velocity, cap,
                  road_deviation,
                  boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed):
         self.name = name
+        self.temperature = temperature
         self.boat_coordinates = boat_coordinates
         self.depth = depth
         self.buoy_coordinates = buoy_coordinates
@@ -33,25 +37,23 @@ class CrewMate:
         self.wind_speed = wind_speed
         self.wind_angles = wind_angles
 
-    @staticmethod
-    def from_pandas(name, dataframe):
-        boat_coordinates = Data.BoatGPSCoordinates.from_pandas(dataframe)
-        depth = Data.Depth.from_pandas(dataframe)
-        buoy_coordinates = Data.BuoyCoordinates.from_pandas(dataframe)
-        temperature = Data.Temperature.from_pandas(dataframe)
-        stream_velocity = Data.StreamVelocity.from_pandas(dataframe)
-        cap = Data.Cap.from_pandas(dataframe)
-        road_deviation = Data.RoadDeviation.from_pandas(dataframe)
-        boat_angles = Data.BoatAngles.from_pandas(dataframe)
-        speed = Data.Speeds.from_pandas(dataframe)
-        flight_height = Data.FlightHeight.from_pandas(dataframe)
-        wing_angles = Data.WingAngles.from_pandas(dataframe)
-        wind_angles = Data.WindAngles.from_pandas(dataframe)
-        wind_speed = Data.WindSpeed.from_pandas(dataframe)
-        pressure = Data.Pressure.from_pandas(dataframe)
-        return CrewMate(name, temperature, boat_coordinates, depth, buoy_coordinates, stream_velocity, cap,
-                        road_deviation,
-                        boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed)
+
+    def from_pandas(self, dataframe):
+        self.boat_coordinates = Data.BoatGPSCoordinates.from_pandas(dataframe)
+        self.depth = Data.Depth.from_pandas(dataframe)
+        self.buoy_coordinates = Data.BuoyCoordinates.from_pandas(dataframe)
+        self.temperature = Data.Temperature.from_pandas(dataframe)
+        self.stream_velocity = Data.StreamVelocity.from_pandas(dataframe)
+        self.cap = Data.Cap.from_pandas(dataframe)
+        self.road_deviation = Data.RoadDeviation.from_pandas(dataframe)
+        self.boat_angles = Data.BoatAngles.from_pandas(dataframe)
+        self.speed = Data.Speeds.from_pandas(dataframe)
+        self.flight_height = Data.FlightHeight.from_pandas(dataframe)
+        self.wing_angles = Data.WingAngles.from_pandas(dataframe)
+        self.wind_angles = Data.WindAngles.from_pandas(dataframe)
+        self.wind_speed = Data.WindSpeed.from_pandas(dataframe)
+
+
 
     def display(self):
         self.road_deviation.print_data()
@@ -73,16 +75,21 @@ class CrewMate:
 
 
 class Helmsman(CrewMate):
-    def __init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+    def __init__(self, name, temperature, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
                  road_deviation,
                  boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed, pressure):
-        super.__init__(self, name, self, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+        super().__init__(name, temperature, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
                        road_deviation,
                        boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed)
-        self.MainWindowHelmsman = QtWidgets.QMainWindow()
+        self.main_window_helmsman = QtWidgets.QMainWindow()
         #Nom de la classe Ui_MainWIndow...
-        self.ui_helmsman = Ui_MainWindowHelmsman
-        self.ui_helmsman.setup_Ui(self.MainWindowHelmsman)
+        #self.ui_helmsman = HelmsmanWindow.Ui_MainWindowHelmsman()
+        #self.ui_helmsman.setupUi(self.main_window_helmsman)
+        self.pressure = pressure
+
+    def from_pandas(self, dataframe):
+        super().from_pandas(dataframe)
+        self.pressure = Data.Pressure.from_pandas(dataframe)
 
 
     def update(self, i):
@@ -104,41 +111,41 @@ class Helmsman(CrewMate):
         self.stream_velocity.print_data()
 
 
-class Foilsman(CrewMate):
-    def __init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
-                 road_deviation,
-                 boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed, pressure):
-        super().__init__(self, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
-                         road_deviation,
-                         boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed)
-
-    def display(self):
-        self.depth.print_data()
-        self.wing_angles.print_data()
-        self.wind_speed.print_data()
-        self.road_deviation.print_data()
-        self.flight_height.print_data()
-        self.buoy_coordinates.print_data()
-        self.speed.print_data()
-        self.boat_coordinates.print_data()
-
-    def update(self, i):
-        self.ui_helmsman.boat_angles_widget.value_gite = self.boat_angles[0][i]
-        self.ui_helmsman.boat_angles.value_tangage = self.boat_angles[1][i]
-
-class Wingsman(CrewMate):
-    def __init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
-                 road_deviation,
-                 boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed, pressure):
-        super().__init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
-                         road_deviation,
-                         boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed)
-
-    def display(self):
-        self.wing_angles.print_data()
-        self.wind_speed.print_data()
-        self.road_deviation.print_data()
-        self.buoy_coordinates.print_data()
-        self.speed.print_data()
-        self.boat_coordinates.print_data()
-
+# class Foilsman(CrewMate):
+#     def __init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+#                  road_deviation,
+#                  boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed, pressure):
+#         super().__init__(self, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+#                          road_deviation,
+#                          boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed)
+#
+#     def display(self):
+#         self.depth.print_data()
+#         self.wing_angles.print_data()
+#         self.wind_speed.print_data()
+#         self.road_deviation.print_data()
+#         self.flight_height.print_data()
+#         self.buoy_coordinates.print_data()
+#         self.speed.print_data()
+#         self.boat_coordinates.print_data()
+#
+#     def update(self, i):
+#         self.ui_helmsman.boat_angles_widget.value_gite = self.boat_angles[0][i]
+#         self.ui_helmsman.boat_angles.value_tangage = self.boat_angles[1][i]
+#
+# class Wingsman(CrewMate):
+#     def __init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+#                  road_deviation,
+#                  boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed, pressure):
+#         super().__init__(self, name, gps_coordinates, depth, buoy_coordinates, stream_velocity, cap,
+#                          road_deviation,
+#                          boat_angles, speed, flight_height, wing_angles, wind_angles, wind_speed)
+#
+#     def display(self):
+#         self.wing_angles.print_data()
+#         self.wind_speed.print_data()
+#         self.road_deviation.print_data()
+#         self.buoy_coordinates.print_data()
+#         self.speed.print_data()
+#         self.boat_coordinates.print_data()
+#
---
+++ Helmsman/premiere_interface_menu_inter.py
@@ -0,0 +1,185 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import gite_widget as GiteWidget
+import WindAnglewidget as WindAngleWidget
+
+
+class Ui_MainWindowHelmsman(object):
+
+    def __init__(self):
+        self.gite_window = QtWidgets.QMainWindow()
+        self.gite_widget = GiteWidget.Ui_GiteWidget()
+        self.wind_angle_window = QtWidgets.QMainWindow()
+        self.wind_angle_widget = WindAngleWidget.Ui_Form()
+
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("HelmsmanWindow")
+        MainWindow.resize(846, 686)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(3)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
+        self.frame.setSizePolicy(sizePolicy)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Light")
+        font.setPointSize(36)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_2.setReadOnly(True)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout_2.addWidget(self.lineEdit_2)
+        self.graphicsView = QtWidgets.QGraphicsView(self.frame)
+        self.graphicsView.setObjectName("graphicsView")
+        # self.initialize_view()
+        self.verticalLayout_2.addWidget(self.graphicsView)
+        self.horizontalLayout.addWidget(self.frame)
+        self.frame_2 = QtWidgets.QFrame(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame_2.sizePolicy().hasHeightForWidth())
+        self.frame_2.setSizePolicy(sizePolicy)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.frame_2)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.frame_2)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Light")
+        font.setPointSize(20)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit.setReadOnly(True)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.scrollArea = QtWidgets.QScrollArea(self.frame_2)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 178, 523))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.pushButton = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 100))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Light")
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout_3.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Light")
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout_3.addWidget(self.pushButton_2)
+        self.pushButton_3 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
+        self.pushButton_3.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Light")
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout_3.addWidget(self.pushButton_3)
+        self.pushButton_4 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
+        self.pushButton_4.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Light")
+        self.pushButton_4.setFont(font)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.verticalLayout_3.addWidget(self.pushButton_4)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout.addWidget(self.scrollArea)
+        self.horizontalLayout.addWidget(self.frame_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 846, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("HelmsmanWindow", "HemlsmanWindow"))
+        self.lineEdit_2.setText(_translate("HelmsmanWindow", "Helmsman"))
+        self.lineEdit.setText(_translate("HelmsmanWindow", "Menu"))
+        self.pushButton.setText(_translate("HelmsmanWindow", "Map - Position"))
+        self.pushButton_2.setText(_translate("HelmsmanWindow", "Depth"))
+        self.pushButton_3.setText(_translate("HelmsmanWindow", "Apparent wind"))
+        self.pushButton_4.setText(_translate("HelmsmanWindow", "VMG"))
+
+
+    def show_gite_widget(self):
+        self.gite_widget.setupUi(self.wind_angle_window)
+        self.wind_angle_window.show()
+
+    def show_wind_angle_widget(self):
+        self.wind_angle_widget.setupUi(self.wind_angle_window)
+        self.wind_angle_window.show()
+
+
+    # def initialize_view(self):
+    #      self.test_scene = QtWidgets.QGraphicsScene(0, 0, 100, 100)
+    #      self.gite = Ui_GiteWidget()
+    #      self.gite.setupUi(self.test_scene)
+    #      self.graphicsView.setScene(self.test_scene)
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_HelmsmanWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+    #Signal managment
+    #=================
+    #Button "map-position"
+    ui.pushButton.clicked.connect(ui.show_gite_widget)
+    #Button "depth"
+    ui.pushButton_2.clicked.connect(ui.show_wind_angle_widget)
+
+
+
+
+    sys.exit(app.exec_())
+
+
