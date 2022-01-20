a35ee8a160498bbb961bdabe3ffba3ed22bddea0
Prunelle Vogler <prunelle-vogler@orange.fr>
2022-01-20 15:19:14 +0100

    ajout du package Foilsman avec le FoilsmanMenu et les 2 widgets de Foilsman

---
+++ Foilsman/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Foilsman',
+      version='0.1',
+      description='Contains the menu for the Foilsman and the associated widgets',
+      author='Inès Larroche - Prunelle Vogler - Even Matencio',
+      author_email='your.name@example.com',
+      license='MIT',
+      packages=['Foilsman'],
+      zip_safe=False)
