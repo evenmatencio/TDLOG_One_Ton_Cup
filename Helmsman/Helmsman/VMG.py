a16c91f6d7a41f3e720b975fd7cb90d834d82f7f
Prunelle Vogler <prunelle-vogler@orange.fr>
2022-01-24 13:54:37 +0100

    Ajout de speed(VMG)

--- Helmsman/Helmsman/VMG.py
+++ Helmsman/Helmsman/VMG.py
@@ -22,9 +22,13 @@ class Ui_VMG(object):
         font.setPointSize(24)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.lcdNumber = QtWidgets.QLCDNumber(Form)
-        self.lcdNumber.setGeometry(QtCore.QRect(330, 440, 64, 23))
-        self.lcdNumber.setObjectName("lcdNumber")
+
+
+        self.vmg_nb = QtWidgets.QLCDNumber(Form)
+        self.vmg_nb.setGeometry(QtCore.QRect(330, 440, 64, 23))
+        self.vmg_nb.setObjectName("lcdNumber")
+        self.vmg_nb.setProperty("intValue", 12)
+
         self.label_3 = QtWidgets.QLabel(Form)
         self.label_3.setGeometry(QtCore.QRect(410, 400, 251, 101))
         font = QtGui.QFont()
@@ -32,14 +36,20 @@ class Ui_VMG(object):
         font.setPointSize(24)
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
+
+
         self.label = QtWidgets.QLabel(Form)
         self.label.setGeometry(QtCore.QRect(40, -10, 671, 591))
         self.label.setText("")
         self.label.setPixmap(QtGui.QPixmap("VMG.png"))
         self.label.setObjectName("label")
-        self.lcdNumber_2 = QtWidgets.QLCDNumber(Form)
-        self.lcdNumber_2.setGeometry(QtCore.QRect(330, 220, 64, 23))
-        self.lcdNumber_2.setObjectName("lcdNumber_2")
+
+        #Angle entre le vent et la vitesse
+        self.wind_angle_nb = QtWidgets.QLCDNumber(Form)
+        self.wind_angle_nb.setGeometry(QtCore.QRect(330, 220, 64, 23))
+        self.wind_angle_nb.setObjectName("lcdNumber_2")
+        self.wind_angle_nb.setProperty("intValue", 45)
+
         self.label_4 = QtWidgets.QLabel(Form)
         self.label_4.setGeometry(QtCore.QRect(400, 190, 41, 81))
         font = QtGui.QFont()
@@ -47,6 +57,8 @@ class Ui_VMG(object):
         font.setPointSize(24)
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
+
+
         self.label.raise_()
         self.label_2.raise_()
         self.lcdNumber.raise_()
@@ -69,7 +81,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_Form()
+    ui = Ui_VMG()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
