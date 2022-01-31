8f32cfb24561e69dedbd97ac7c82a028136c1007
Even <matencio.even@gmail.com>
2022-01-31 09:27:23 +0100

    Ajout du README, intermediaire

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
+###################
+###################
 # TDLOG_One_Ton_Cup
+###################
+###################
+
+
+###########
+# WARNING #
+###########
+
+So as to run the code, you need to install the custom packages "Helmsman", "Foilsman" and "Wingsman" which contain the menus and widgets for each Crewmate.
+For this purpose, you have to enter the following command : 
+
+"pip install -e ."  (don't forget the dot)
+
+in your (anaconda or device) console when located in the folder "Helmsman" (respectively "Foilsman" and "Wingsman"), containing the file "setup.py"
+for the Helmsman package (respectively the Foilsman and Wingsman packages).
+
+
+###################################
+# Main caracteristics of the code #
+###################################
+
+Origin of the data:
+###################
+
+The current version of the code is based on a dataset provided by the .csv file "Random_data_x", where "x" indicates the number of figures given 
+for each kind of data. Most of the data contained in this file are unused but can serve as examples for an additional displayed data to one of the crewmates.
+
+Using a .csv file leads to a specific managment of the data. Basically, any function or method whose identifier contains "from_pandas" is dedicated 
+to this, but there always exists a generic method (typically for constructors) that does not depend on the origin of the data. Nonetheless, these functions /
+methods should be written again according to the container of the data used.
+
+
+
+
+######################################
+# Necessary improvements of the code #
+######################################
+
+
+
--- TDL_OTC_Data.py
+++ TDL_OTC_Data.py
@@ -1,4 +1,5 @@
 from queue import Queue
+import numpy
 
 """ This module contains the parent Data class for the OTC software project
  and the subclasses related to the different data used in the software """
@@ -10,151 +11,121 @@ class OneTonCupData:
 
 
 class BoatGPSCoordinates(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[['Longitude', 'Latitude']]
+        data_import = numpy.asarray(dataframe[['Longitude', 'Latitude']])
         return BoatGPSCoordinates(data_import)
 
 
 class Depth(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Profondeur"]
+        data_import = numpy.asarray(dataframe["Profondeur"])
         return Depth(data_import)
 
 
 class BuoyCoordinates(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[['Latitude bouee 1', 'Longitude bouee 1', 'Latitude bouee 2', 'Longitude bouee 2',
-                                 'Latitude bouee 3', 'Longitude bouee 3', 'Latitude bouee 4', 'Longitude bouee 4']]
+        data_import = numpy.asarray(dataframe[['Latitude bouee 1', 'Longitude bouee 1', 'Latitude bouee 2', 'Longitude bouee 2',
+                                 'Latitude bouee 3', 'Longitude bouee 3', 'Latitude bouee 4', 'Longitude bouee 4']])
         return BuoyCoordinates(data_import)
 
 
 class Temperature(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Temperature"]
+        data_import = numpy.asarray(dataframe["Temperature"])
         return Temperature(data_import)
 
 
 class StreamVelocity(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Vitesse courant"]
+        data_import = numpy.asarray(dataframe["Vitesse courant"])
         return StreamVelocity(data_import)
 
 
 class Cap(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Cap"]
+        data_import = numpy.asarray(dataframe["Cap"])
         return Cap(data_import)
 
 
 class RoadDeviation(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Ecart a la route"]
+        data_import = numpy.asarray(dataframe["Ecart a la route"])
         return RoadDeviation(data_import)
 
 
 class BoatAngles(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[["Angle de gite", "Angle de tangage"]]
+        data_import = numpy.asarray(dataframe[["Angle de gite", "Angle de tangage"]])
         return BoatAngles(data_import)
 
 
 class Speeds(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[["Vitesse bateau", "VMG"]]
+        data_import = numpy.asarray(dataframe[["Vitesse bateau", "VMG"]])
         return Speeds(data_import)
 
 
 class FlightHeight(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Hauteur de vol"]
+        data_import = numpy.asarray(dataframe["Hauteur de vol"])
         return FlightHeight(data_import)
 
 
 class WingAngles(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[["Vrillage angle 1", "Vrillage angle 2", "Cambrure aile"]]
+        data_import = numpy.asarray(dataframe[["Vrillage angle 1", "Vrillage angle 2", "Cambrure aile"]])
         return WingAngles(data_import)
 
 
 class WindAngles(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[["Angle au vent reel", "Angle au vent apparent"]]
+        data_import = numpy.asarray(dataframe[["Angle au vent reel", "Angle au vent apparent"]])
         return WindAngles(data_import)
 
 
 class WindSpeed(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe[["Vitesse du vent", "Beaufort"]]
+        data_import = numpy.asarray(dataframe[["Vitesse du vent", "Beaufort"]])
         return WindSpeed(data_import)
 
 
 class Pressure(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Pression atmospherique"]
+        data_import = numpy.asarray(dataframe["Pression atmospherique"])
         return Pressure(data_import)
     
 class BatteryPowerLevel(OneTonCupData):
-    def __init__(self, data_import):
-        super().__init__(data_import)
 
     @staticmethod
     def from_pandas(dataframe):
-        data_import = dataframe["Niveau batterie"]
+        data_import = numpy.asarray(dataframe["Niveau batterie"])
         return BatteryPowerLevel(data_import)
     
